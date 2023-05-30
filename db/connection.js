525736f24bf811b4a178687f3650039ae5b9a760
Jodie Cheung <jodiecheung@Jodies-MBP.broadband>
2023-05-30 14:54:52 +0100

    listen

--- db/connection.js
+++ db/connection.js
@@ -9,12 +9,10 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-const config =
-  ENV === "production"
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        max: 2,
-      }
-    : {};
+const config = {};
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
 module.exports = new Pool(config);
